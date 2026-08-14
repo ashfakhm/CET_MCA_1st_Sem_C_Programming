a278d1d0071b0078c99c507be6c0b0314dd4f4e9
ASHFAKH M <ashfakhthedev@gmail.com>
2026-08-14 22:36:14 +0530

    feat(ads): add 03_AddTwoNumber.c program

---
+++ 03_AddTwoNumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    double number1, number2;
+    printf("Please Enter The Two Numbers You Want To Know The Sums");
+
+    printf("\nType Number 1 : ");
+    scanf("%lf", &number1);
+
+    printf("Type Number 2 : ");
+    scanf("%lf", &number2);
+
+    double sum;
+    sum = number1 + number2;
+
+    printf("The Sum Of Two Numbers Is %d", sum);
+}
